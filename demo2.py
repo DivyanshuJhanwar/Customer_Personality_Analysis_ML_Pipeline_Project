547b0a3dbe2cb1c5a848b0ff1e400ffbc82c1fc6
divyanshu <jhanwar.divyanshu@gmail.com>
2023-09-05 19:35:55 +0530

    Application tested and Successfully completed

--- demo2.py
+++ demo2.py
@@ -4,23 +4,29 @@ from customer_personality.logger import log
 from customer_personality.components.data_validation import DataValidation
 from customer_personality.components.data_transformation import DataTransformation
 from customer_personality.components.model_training import ModelTrainer
+from customer_personality.pipeline.training_pipeline import TrainingPipeline
 
 
-#obj = DataIngestion()
-#obj.initiate_data_ingestion()
-#print("Data Ingestion Completed!")
+obj = DataIngestion()
+obj.initiate_data_ingestion()
+print("Data Ingestion Completed!")
 
 
-#obj = DataValidation()
-#obj.initiate_data_validation()
-#print("Data Validation Completed!")
+obj = DataValidation()
+obj.initiate_data_validation()
+print("Data Validation Completed!")
 
 
-#obj = DataTransformation()
-#obj.initiate_data_transformation()
-#print("Data Transformation Completed!")
+obj = DataTransformation()
+obj.initiate_data_transformation()
+print("Data Transformation Completed!")
 
 
 obj = ModelTrainer()
 obj.initiate_model_trainer()
 print("Model Training Completed!")
+
+
+obj = TrainingPipeline()
+obj.initiate_training_pipeline()
+print("Training Completed!")
